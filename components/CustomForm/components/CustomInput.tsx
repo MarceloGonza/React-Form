93b65c950f7d78cac10485622569657dd9cbcac4
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-05-14 11:28:26 -0300

    form

---
+++ components/CustomForm/components/CustomInput.tsx
@@ -0,0 +1,31 @@
+import { FieldError, Control, Controller } from "react-hook-form";
+import "./CustomInput.css";
+
+interface Props {
+  name: string;
+  control: Control<any>;
+  label: string;
+  type?: string;
+  error?: FieldError;
+}
+
+export const InputForm = ({ name, control, label, type, error }: Props) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <input
+            id={name}
+            type={type}
+            {...field}
+            className={`form-control ${error ? "is-invalid" : ""}`}
+          />
+        )}
+      />
+      {error && <p className="error">{error.message}</p>}
+    </div>
+  );
+};
